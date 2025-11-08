a72ba93faf79dd48b8a2ba997112287e2904073a
KeirrysonMatheus <keirrysonmatheusrodriguez@gmail.com>
2025-11-07 22:34:43 -0300

    Implement smooth scrolling in showSection function
    Add smooth scrolling to showSection function.

--- js/index.js
+++ js/index.js
@@ -2,6 +2,7 @@ function showSection(id) {
       const sections = document.querySelectorAll('.section');
       sections.forEach(section => section.classList.remove('active'));
       document.getElementById(id).classList.add('active');
+      window.scrollTo({ top: 250, behavior: 'smooth' });
     }
 
     const dataInicio = new Date('2024-01-17T15:20:00');
